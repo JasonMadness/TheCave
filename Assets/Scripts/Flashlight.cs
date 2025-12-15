59cd758f070684065d4a74db4dc81d07e00097c9
JasonMadness <tobc_madness@mail.ru>
2025-12-15 21:31:05 +0300

    flashlight
    added flashlight with smooth delay movement
    decrease volume lightning intensity

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    [Header("Параметры плавности")]
+    [SerializeField] private float _horizontalSmoothSpeed = 4f;  // Скорость плавности по горизонтали (Y)
+    [SerializeField] private float _verticalSmoothSpeed = 4f;    // Скорость плавности по вертикали (X)
+    [SerializeField] private float _positionSmoothSpeed = 4f;    // Скорость плавности позиции
+
+    private Vector3 _offsetFromCamera;           // Смещение фонарика от камеры
+    private GameObject _followTarget;            // Объект, за которым следует фонарик (камера)
+    private float _currentYaw;                   // Текущий горизонтальный угол (Y)
+    private float _targetYaw;                    // Целевой горизонтальный угол
+    private float _currentPitch;                 // Текущий вертикальный угол (X)
+    private float _targetPitch;                  // Целевой вертикальный угол
+
+    private void Start()
+    {
+        _followTarget = Camera.main.gameObject;
+        _offsetFromCamera = transform.position - _followTarget.transform.position;
+
+        // Инициализируем углы
+        Vector3 eulerAngles = _followTarget.transform.eulerAngles;
+        _targetYaw = eulerAngles.y;
+        _currentYaw = _targetYaw;
+        _targetPitch = eulerAngles.x;
+        _currentPitch = _targetPitch;
+    }
+
+    private void LateUpdate()
+    {
+        // Обновляем целевые углы
+        Vector3 eulerAngles = _followTarget.transform.eulerAngles;
+        _targetYaw = eulerAngles.y;
+        _targetPitch = eulerAngles.x;
+
+        // Плавно интерполируем углы
+        _currentYaw = Mathf.LerpAngle(_currentYaw, _targetYaw, _horizontalSmoothSpeed * Time.deltaTime);
+        _currentPitch = Mathf.LerpAngle(_currentPitch, _targetPitch, _verticalSmoothSpeed * Time.deltaTime);
+
+        // Применяем новые углы
+        Quaternion newRotation = Quaternion.Euler(_currentPitch, _currentYaw, 0);
+        transform.rotation = newRotation;
+
+        // Плавное движение позиции
+        Vector3 desiredPosition = _followTarget.transform.position + _offsetFromCamera;
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, _positionSmoothSpeed * Time.deltaTime);
+    }
+}
